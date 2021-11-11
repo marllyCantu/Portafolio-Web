427c94094bc7576c8c4227547474a5f32a116f62
marllyCantu <1930064@upv.edu.mx>
2021-11-10 23:41:42 -0600

    Arreglo menor

--- Unidad 3/roux_academy/roux_academy/layouts/footer.php
+++ Unidad 3/roux_academy/roux_academy/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="branding"> <img src="/roux_academy/roux_academy/images/ralogo_monogram.png" alt="Logo"> </div>
+    <div class="branding"> <img src="images/ralogo_monogram.png" alt="Logo"> </div>
     <div class="description">Join over 500 hundred of the most creative and brilliant minds of art colleges all around the world for three days of lectures by world-renowned art scholars and artists.</div>
     <nav>
       <ol>
